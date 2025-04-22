16aefab0e041957dd3a558ab22524b00ad8f4b50
yussufnarcos <narcosyussuf@gmail.com>
2025-04-22 11:16:35 +0300

    react-hooks-components-basics-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <h1>My Portfolio</h1>
     </nav>
   );
 }
@@ -11,20 +11,25 @@ function NavBar() {
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h2>Welcome to my portfolio</h2>
     </div>
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
